ab175cf670b8e11089845afd2f7bb501a425aa27
Naomi Luis <nluis@quicinc.com>
2009-11-11 15:05:33 -0800

    vendor/qcom/msm7627_surf: Add BOARD_EGL_CFG flag

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -49,3 +49,4 @@ BOARD_KERNEL_BASE    := 0x00200000
 BOARD_NAND_PAGE_SIZE := 2048
 
 BOARD_KERNEL_CMDLINE := mem=203M console=ttyDCC0 androidboot.hardware=qcom
+BOARD_EGL_CFG := vendor/qcom/$(TARGET_PRODUCT)/egl.cfg
