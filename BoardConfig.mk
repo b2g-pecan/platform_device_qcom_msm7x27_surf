a4e6e62e7124024910d607d562fb3717126ee19d
Priya Komarlingam <knspriya@quicinc.com>
2010-01-18 15:30:58 -0800

    msm7627_surf: Increase available memory to 213M

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -50,7 +50,7 @@ TARGET_BOARD_PLATFORM := msm7k
 BOARD_KERNEL_BASE    := 0x00200000
 BOARD_NAND_PAGE_SIZE := 2048
 
-BOARD_KERNEL_CMDLINE := mem=203M console=ttyDCC0 androidboot.hardware=qcom
+BOARD_KERNEL_CMDLINE := mem=213M console=ttyDCC0 androidboot.hardware=qcom
 BOARD_EGL_CFG := vendor/qcom/$(TARGET_PRODUCT)/egl.cfg
 
 BOARD_NO_SPEAKER := true # msm7627_surf doesn't have speaker
